583be73af9b2e77d4a9fadcb6519267a3c9ec7a6
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-06-29 15:39:32 +0100

    Update index and event show pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,6 +64,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :date,)
+      params.require(:event).permit(:title, :date)
     end
 end
