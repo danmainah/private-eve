f070387fc585f904aa7aa6528958e98f295b2e9f
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-06-30 16:09:31 +0100

    Add testing gems to Gemfile

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,8 @@ class Event < ApplicationRecord
     has_many :event_attendees, foreign_key: 'event_id'
     has_many :attendees, through: :event_attendees, source: :attendee
 
-    scope :past, -> { where("date < ?",Time.current)}
+    scope :past, -> { where("date < ?", Time.current) }
 
-    scope :future, -> {where("date > ?",Time.current)}
+    scope :future, -> {where("date > ?", Time.current) }
    
 end
