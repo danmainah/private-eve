1c28eb65bf71ac50456a4c5f15aba99c4765c734
unknown <danielmainawaweru@gmail.com>
2021-06-30 19:37:42 +0300

    Set up code linter

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,11 +1,11 @@
 class EventAttendeesController < ApplicationController
   def create
-      @event = Event.find(params[:event_id])
-      @event_attended = @event.event_attendees.build(attendee_id: current_user.id)
-       if @event_attended.save
-            redirect_to event_path(params[:event_id])
-        else
-            render body: "not saved"
-        end
+    @event = Event.find(params[:event_id])
+    @event_attended = @event.event_attendees.build(attendee_id: current_user.id)
+    if @event_attended.save
+      redirect_to event_path(params[:event_id])
+    else
+      render body: 'not saved'
     end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,10 @@
 class Event < ApplicationRecord
-    belongs_to :creator, class_name: 'User'
+  belongs_to :creator, class_name: 'User'
 
-    has_many :event_attendees, foreign_key: 'event_id'
-    has_many :attendees, through: :event_attendees, source: :attendee
+  has_many :event_attendees, foreign_key: 'event_id'
+  has_many :attendees, through: :event_attendees, source: :attendee
 
-    scope :past, -> { where("date < ?", Time.current) }
+  scope :past, -> { where('date < ?', Time.current) }
 
-    scope :future, -> {where("date > ?", Time.current) }
-   
+  scope :future, -> { where('date > ?', Time.current) }
 end
