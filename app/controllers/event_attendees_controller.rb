2729cd5d29ddb80ccb804175cdd65ca212c310de
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-07-01 10:44:31 +0100

    Fix rubocop errors

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,13 +1,13 @@
 class EventAttendeesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    users_events= @event.event_attendees.find_by(attendee_id: current_user.id)
-      redirect_to event_path(params[:event_id]) and return if users_events 
+
+    users_events = @event.event_attendees.find_by(attendee_id: current_user.id)
+    redirect_to event_path(params[:event_id]) and return if users_events
+
     @event_attended = @event.event_attendees.build(attendee_id: current_user.id)
-    if @event_attended.save 
-      redirect_to event_path(params[:event_id]) and  return
-    else
-      render body: 'not saved'
-    end
+    redirect_to event_path(params[:event_id]) and return if @event_attended.save
+
+    render body: 'not saved'
   end
 end
